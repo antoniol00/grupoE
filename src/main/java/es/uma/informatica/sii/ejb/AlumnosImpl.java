b514aec557d2a53bd527537e6ad054b13e299b15
alumno <antoniolara2000@uma.es>
2021-04-19 09:28:00 -0700

    alumnos_ejb provisional implementation

--- src/main/java/es/uma/informatica/sii/ejb/AlumnosImpl.java
+++ src/main/java/es/uma/informatica/sii/ejb/AlumnosImpl.java
@@ -1,32 +1,139 @@
 package es.uma.informatica.sii.ejb;
 
-import java.util.logging.Logger;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 import es.uma.informatica.sii.ejb.exceptions.SecretariaException;
 import es.uma.informatica.sii.entities.Alumno;
+import es.uma.informatica.sii.entities.Expediente;
+import es.uma.informatica.sii.entities.Titulacion;
 
 @Stateless
 public class AlumnosImpl implements GestionAlumnos {
 
-	private static final Logger LOG = Logger.getLogger(AlumnosImpl.class.getCanonicalName());
-
 	@PersistenceContext(name = "grupoE")
 	private EntityManager em;
 
+	/**
+	 * El fichero alumnos se encuentra en ./DATOS/alumnos.csv Para completar la
+	 * tabla ALUMNOS esta sería la correspondencia entre columnas del csv y excel:
+	 * CSV -> BBDD DOCUMENTO -> DNI NOMBRE+APELLIDO1+APELLIDO2 -> NOMBRE_COMPLETO
+	 * EMAIL_INSTITUCIONAL -> EMAIL_INSTITUCIONAL EMAIL_PERSONAL -> EMAIL_PERSONAL
+	 * TELEFONO -> TELEFONO_FIJO MOVIL -> TELEFONO_MOVIL DIRECCION_NOTIFICACION ->
+	 * DIRECCION_NOTIFICACION LOCALIDAD_NOTIFICACION -> LOCALIDAD_NOTIFICACION
+	 * PROVINCIA_NOTIFICACION -> PROVINCIA_NOTIFICACION CODIGO_POSTAL_NOTIFICACION
+	 * -> CODIGO_POSTAL
+	 * 
+	 * @throws IOException         si hay un error en el acceso a alumnos.csv
+	 * @throws SecretariaException si no encuentra en la bbdd alguna referencia a
+	 *                             otra entidad
+	 * 
+	 */
 	@Override
-	public void importaAlumnos(String file) throws SecretariaException {
-		// TODO Auto-generated method stub
+	public void importaAlumnos(String file) throws SecretariaException, IOException {
+		// string de cabeceras para archivo alumnos.csv
+		String[] HEADERS = { "DOCUMENTO", "NOMBRE", "APELLIDO1", "APELLIDO2", "EXPEDIENTE", "ARCHIVO", "EMAIL_INST",
+				"EMAIL_PER", "FIJO", "MOVIL", "DIR", "LOCAL", "PRO", "CP", "FECHA", "TURNO", "GRUPOS", "NOTA",
+				"CREDITOS", "CREDITOS_FB", "CREDITOS_OB", "CREDITOS_OP", "CREDITOS_CF", "CREDITOS_PE", "CREDITOS_TF" };
+
+		// RELLENAMOS TABLA ALUMNOS
+		Reader in = new FileReader("./DATOS/alumnos.csv");
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader(HEADERS).withDelimiter(';').parse(in);
+		int x = 0;
+		for (CSVRecord record : records) {
+			// eliminamos las primeras 3 lineas de basura + header
+			if (x > 3) {
+				String dni = record.get("DOCUMENTO");
+				String nombre_completo = record.get("NOMBRE") + " " + record.get("APELLIDO1") + " "
+						+ record.get("APELLIDO2");
+				String email_inst = record.get("EMAIL_INST");
+				String email_per = record.get("EMAIL_PER");
+				String fijo = record.get("FIJO");
+				String movil = record.get("MOVIL");
+				String dir_not = record.get("DIR");
+				String loc_not = record.get("LOCAL");
+				String pro_not = record.get("PRO");
+				String cp = record.get("CP");
+
+				Alumno al = new Alumno();
+				al.setDni(dni);
+				al.setNombre_completo(nombre_completo);
+				al.setEmail_institucional(email_inst);
+				al.setEmail_personal(email_per);
+				al.setTelefono_fijo(Integer.parseInt(fijo.replaceAll(" ", "")));
+				al.setTelefono_movil(Integer.parseInt(movil.replaceAll(" ", "")));
+				al.setDireccion_notificacion(dir_not);
+				al.setLocalidad_notificacion(loc_not);
+				al.setProvincia_notificacion(pro_not);
+				al.setCodigo_postal(Integer.parseInt(cp));
+
+				// introducimos en la base de datos cada alumno
+				em.persist(al);
+
+			} else {
+				x++;
+			}
+		}
+
+		// RELLENAMOS TABLA EXPEDIENTE
+		Reader in2 = new FileReader("./DATOS/alumnos.csv");
+		Iterable<CSVRecord> records2 = CSVFormat.DEFAULT.withHeader(HEADERS).withDelimiter(';').parse(in2);
+		int y = 0;
+		for (CSVRecord record : records2) {
+			// eliminamos las primeras 3 lineas de basura + header
+			if (y > 3) {
+				String numero = record.get("EXPEDIENTE");
+				String nota = record.get("NOTA");
+				String creditos_fb = record.get("CREDITOS_FB");
+				String creditos_ob = record.get("CREDITOS_OB");
+				String creditos_pe = record.get("CREDITOS_PE");
+				String creditos_tf = record.get("CREDITOS_TF");
+				String dni = record.get("DOCUMENTO");
+
+				Expediente ex = new Expediente();
+				ex.setNumero(Integer.parseInt(numero));
+				ex.setActivo(true);
+				ex.setNota_media(Double.parseDouble(nota));
+				ex.setCreditos_fb(Double.parseDouble(creditos_fb));
+				ex.setCreditos_ob(Double.parseDouble(creditos_ob));
+				ex.setCreditos_pe(Double.parseDouble(creditos_pe));
+				ex.setCreditos_tf(Double.parseDouble(creditos_tf));
+				Titulacion t = em.find(Titulacion.class, numero.substring(0, 3));
+				if (t == null) {
+					throw new SecretariaException("Se ha intentado asignar un codigo de titulacion que no existe");
+				}
+				ex.setTitulacion(t);
+				Alumno al = em.find(Alumno.class, dni);
+				if (al == null) {
+					throw new SecretariaException("Se ha intentado asignar un alumno que no existe");
+				}
+				ex.setAlumno(al);
+
+				// introducimos en la base de datos cada expediente
+				em.persist(al);
+
+			} else {
+				y++;
+			}
+		}
 
 	}
 
 	@Override
 	public void modificaAlumno(String dni_alumno, Alumno al) throws SecretariaException {
-		// TODO Auto-generated method stub
-
+		Alumno alumno_entity = em.find(Alumno.class, dni_alumno);
+		if (alumno_entity == null) {
+			throw new SecretariaException("Se ha intentado modificar un alumno que no existe");
+		}
+		em.merge(al);
 	}
 
 }
--- src/main/java/es/uma/informatica/sii/ejb/GestionAlumnos.java
+++ src/main/java/es/uma/informatica/sii/ejb/GestionAlumnos.java
@@ -1,5 +1,7 @@
 package es.uma.informatica.sii.ejb;
 
+import java.io.IOException;
+
 import javax.ejb.Local;
 
 import es.uma.informatica.sii.ejb.exceptions.SecretariaException;
@@ -14,15 +16,20 @@ public interface GestionAlumnos {
 	 * alumno en la tabla ALUMNOS como la de la tabla EXPEDIENTE. El acceso
 	 * y procesamiento de estos datos se realiza mediante el fichero alumnos.csv proporcionado
 	 * 
-	 * @param file: ruta del archivo donde importarS
+	 * @param file: ruta del archivo donde importar
+	 * 
+	 * @throws IOException si hay un error en el acceso a alumnos.csv
+	 * @throws SecretariaException si no encuentra en la bbdd alguna referencia a otra entidad
 	 */
-	public void importaAlumnos(String file) throws SecretariaException;
+	public void importaAlumnos(String file) throws SecretariaException, IOException;
 	
 	/**
 	 * Modifica el alumno pasado como parametro e identificado por su DNI
 	 * 
 	 * @param dni_alumno: alumno que se desea modificar
 	 * @param al: informacion actualizada del alumno
+	 * 
+	 * @throws SecretariaException si no encuentra en la bbdd alguna referencia al alumno a modificar
 	 */
 	public void modificaAlumno(String dni_alumno, Alumno al) throws SecretariaException;
 }
