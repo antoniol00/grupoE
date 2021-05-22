6e125713e53a24325b1e2f03b27f2c6e2cb3e001
antoniol00 <antoniolara2000@uma.es>
2021-05-22 17:03:58 +0200

    new fixes for AlumnosBean

--- grupoE-war/src/main/java/es/uma/informatica/sii/backing/AlumnosBean.java
+++ grupoE-war/src/main/java/es/uma/informatica/sii/backing/AlumnosBean.java
@@ -1,8 +1,8 @@
 package es.uma.informatica.sii.backing;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Date;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -12,7 +12,6 @@ import javax.servlet.http.Part;
 
 import es.uma.informatica.sii.ejb.AlumnosImpl;
 import es.uma.informatica.sii.ejb.GestionAlumnos;
-import es.uma.informatica.sii.ejb.GestionPeticiones;
 import es.uma.informatica.sii.ejb.exceptions.SecretariaException;
 import es.uma.informatica.sii.ejb.exceptions.SecretariaIOException;
 import es.uma.informatica.sii.entities.Alumno;
@@ -24,16 +23,6 @@ public class AlumnosBean {
 	private GestionAlumnos al = new AlumnosImpl();
 
 	private Alumno alum;
-	private String DNI;
-	private String nombre;
-	private String email_inst;
-	private String email_pers;
-	private Integer telef_fijo;
-	private Integer telef_mov;
-	private String direc;
-	private String localidad;
-	private String provincia;
-	private Integer codPost;
 
 	private Part part;
 	private String fileName;
@@ -46,104 +35,6 @@ public class AlumnosBean {
 		return al.obtenerListaAlumnos();
 	}
 
-	/*public String crearPeticion() throws SecretariaException, SecretariaIOException, IOException {
-		p.setDate(new Date());
-		p.setAlumno(al.obtenerAlumno(DNI));
-		pet.creaIncidencia(p);
-		p = new Peticion();
-		return null;
-	}
-	 */
-	public String getAsignarDNI() {
-		return DNI;
-	}
-
-	public void setAsignarDNI(String dni) {
-		this.DNI = dni;
-	}
-
-	
-	public String getAsignarNombre() {
-		return nombre;
-	}
-
-	public void setAsignarNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	
-	public String getAsignarEmailInst() {
-		return email_inst;
-	}
-
-	public void setAsignarEmailInst(String email_inst) {
-		this.email_inst = email_inst;
-	}
-	
-	
-	public String getAsignarEmailPers() {
-		return email_pers;
-	}
-
-	public void setAsignarEmailPers(String email_pers) {
-		this.email_pers = email_pers;
-	}
-	
-	
-	public Integer getAsignarTelFij() {
-		return telef_fijo;
-	}
-
-	public void setAsignarTelFij(Integer telef_fijo) {
-		this.telef_fijo = telef_fijo;
-	}
-	
-	
-	public Integer getAsignarTelMov() {
-		return telef_mov;
-	}
-
-	public void setAsignarTelMov(Integer telef_mov) {
-		this.telef_mov = telef_mov;
-	}
-	
-	
-	public String getAsignarDireccion() {
-		return direc;
-	}
-
-	public void setAsignarDireccion(String direc) {
-		this.direc = direc;
-	}
-	
-	
-	public String getAsignarLocal() {
-		return localidad;
-	}
-
-	public void setAsignarLocal(String localidad) {
-		this.localidad = localidad;
-	}
-	
-	
-	public String getAsignarProv() {
-		return provincia;
-	}
-
-	public void setAsignarProv(String provincia) {
-		this.provincia = provincia;
-	}
-	
-	
-	public Integer getAsignarCP() {
-		return codPost;
-	}
-
-	public void setAsignarCP(Integer codPost) {
-		this.codPost = codPost;
-	}
-	
-	
 	public Part getPart() {
 		return part;
 	}
@@ -152,7 +43,6 @@ public class AlumnosBean {
 		this.part = part;
 	}
 
-	
 	public String getFileName() {
 		return fileName;
 	}
@@ -161,17 +51,21 @@ public class AlumnosBean {
 		this.fileName = fileName;
 	}
 
-	
 	public Alumno getAlumno() {
 		return alum;
 	}
 
 	public String upload() throws IOException, SecretariaIOException, SecretariaException {
 
-		fileName = Paths.get(part.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+		fileName = Paths.get(part.getSubmittedFileName()).getFileName().toString();
 		part.write(fileName);
-		al.importaAlumnos(System.getProperty("jboss.server.temp.dir")
-				+ "/grupoE-ear-0.0.1-SNAPSHOT.ear.grupoE-war-0.0.1-SNAPSHOT.war/" + fileName);
+		String ruta = System.getProperty("jboss.server.temp.dir")
+				+ "/grupoE-ear-0.0.1-SNAPSHOT.ear.grupoE-war-0.0.1-SNAPSHOT.war/" + fileName;
+		al.importaAlumnos(ruta);
+		al.importaExpedientes(ruta);
+		File file = new File(ruta);
+		file.delete();
+
 		return null;
 	}
 }
--- grupoE-war/src/main/java/es/uma/informatica/sii/backing/PeticionesBean.java
+++ grupoE-war/src/main/java/es/uma/informatica/sii/backing/PeticionesBean.java
@@ -1,21 +1,17 @@
 package es.uma.informatica.sii.backing;
 
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.Date;
 import java.util.List;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
-import javax.servlet.http.Part;
 
 import es.uma.informatica.sii.ejb.AlumnosImpl;
 import es.uma.informatica.sii.ejb.GestionAlumnos;
 import es.uma.informatica.sii.ejb.GestionPeticiones;
 import es.uma.informatica.sii.ejb.PeticionesImpl;
 import es.uma.informatica.sii.ejb.exceptions.SecretariaException;
-import es.uma.informatica.sii.ejb.exceptions.SecretariaIOException;
 import es.uma.informatica.sii.entities.Peticion;
 
 @Named
@@ -29,8 +25,6 @@ public class PeticionesBean {
 
 	private Peticion p;
 	private String DNI;
-	private Part part;
-	private String fileName;
 
 	public PeticionesBean() {
 		p = new Peticion();
@@ -40,11 +34,16 @@ public class PeticionesBean {
 		return pet.listaPeticiones();
 	}
 
-	public String crearPeticion() throws SecretariaException, SecretariaIOException, IOException {
+	public String crearPeticion() throws SecretariaException {
 		p.setDate(new Date());
 		p.setAlumno(al.obtenerAlumno(DNI));
 		pet.creaIncidencia(p);
 		p = new Peticion();
+		return null;
+	}
+
+	public String eliminarPeticion(Peticion peticion) throws SecretariaException {
+		pet.borraIncidencia(peticion.getDate(), peticion.getAlumno().getDni());
 		return null;
 	}
 
@@ -56,33 +55,8 @@ public class PeticionesBean {
 		this.DNI = dni;
 	}
 
-	public Part getPart() {
-		return part;
-	}
-
-	public void setPart(Part part) {
-		this.part = part;
-	}
-
-	public String getFileName() {
-		return fileName;
-	}
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-
 	public Peticion getPeticion() {
 		return p;
-	}
-
-	public String upload() throws IOException, SecretariaIOException, SecretariaException {
-
-		fileName = Paths.get(part.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
-		part.write(fileName);
-		al.importaAlumnos(System.getProperty("jboss.server.temp.dir")
-				+ "/grupoE-ear-0.0.1-SNAPSHOT.ear.grupoE-war-0.0.1-SNAPSHOT.war/" + fileName);
-		return null;
 	}
 
 }
