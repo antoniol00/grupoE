c236b2232f63c3ac789c39c17785ea5f81dbe5bf
vrm2000 <vrm2000@uma.es>
2021-06-07 21:15:23 +0200

    added MatriculasIntegracion tests

--- grupoE-tests/src/test/java/es/uma/informatica/sii/web/tests/SuiteIT.java
+++ grupoE-tests/src/test/java/es/uma/informatica/sii/web/tests/SuiteIT.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PeticionesIntegracion.class, AlumnosIntegracion.class, AsignaturasIntegracion.class })
+@SuiteClasses({ /*PeticionesIntegracion.class, AlumnosIntegracion.class, AsignaturasIntegracion.class*/ MatriculasIntegracion.class })
 public class SuiteIT {
 	@BeforeClass
 	public static void before() {
